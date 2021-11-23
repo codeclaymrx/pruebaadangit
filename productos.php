66f5f13781ec2a009eaa304c200e3643e3d08012
codeclaymrx <luciodavidsf@gmail.com>
2021-11-23 16:08:40 -0600

    esta es la parte de los productos ya terminado

--- productos.php
+++ productos.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Este es el nuevo titulo - del negocio</title>
+    <title>Esta es la seccion de productos</title>
 </head>
 <body>
-    
+    <h1>Producto 1</h1>
 </body>
 </html>
